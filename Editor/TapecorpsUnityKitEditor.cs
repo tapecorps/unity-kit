f921ef0f51cd45ca1968d2d4fde8ec901415da73
Ahmet Erdoğan Akkulak <koronerofficial@gmail.com>
2023-12-18 11:43:49 +0300

    dev-time-1

---
+++ Editor/TapecorpsUnityKitEditor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public static class TapecorpsUnityKitEditor{
+
+        #if UNITY_EDITOR
+        public static T[] GetAllInstances<T>() where T : ScriptableObject
+        {
+            string[] guids = AssetDatabase.FindAssets("t:" + typeof(T).Name);
+            T[] a = new T[guids.Length];
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+                a[i] = AssetDatabase.LoadAssetAtPath<T>(path);
+            }
+
+            return a;
+        }
+
+        #endif
+}
